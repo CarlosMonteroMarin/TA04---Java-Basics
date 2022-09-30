fc5548aad4b217ddf6ae81436a514d4468e1c944
unknown <CarlosMonteroMarin@carlosmonteromarin@gmail.com>
2022-09-30 11:54:05 +0200

    Commit 1

---
+++ TA04_Java Basics/src/Ejercicio1.java
@@ -0,0 +1,17 @@
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		double num1=5, num2=3;
+		//suma
+		System.out.println("Suma: "+(num1+num2));
+		//resta
+		System.out.println("Resta: "+(num1-num2));
+		//multiplicacion
+		System.out.println("Multiplicación: "+(num1*num2));
+		//division
+		System.out.println("División: "+(num1/num2));
+	
+	}
+
+}
---
+++ TA04_Java Basics/src/Ejercicio2.java
@@ -0,0 +1,23 @@
+
+public class Ejercicio2 {
+
+	public static void main(String[] args) {
+		//variables
+		int N=7;
+		double A=22.8;
+		char C='A';
+	
+		//Impresion por pantalla de variables
+		System.out.println("Variable N="+N);
+		System.out.println("Variable A="+A);
+		System.out.println("Variable C="+C);
+	
+		//suma
+		System.out.println(N+" + "+A+" = "+(N+A));
+		//Resta
+		System.out.println(N+" + "+A+" = "+(N+A));
+
+
+	}
+
+}
